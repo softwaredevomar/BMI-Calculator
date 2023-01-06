6391b4023310abdf4a449991816b1f3925feda66
Omar Ahmed <developeromi@icloud.com>
2023-01-05 18:39:26 -0800

    Updated files.

---
+++ BMI Calculator/Controllers/BMI.swift
@@ -0,0 +1,15 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Omar Ahmed on 1/5/23.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
